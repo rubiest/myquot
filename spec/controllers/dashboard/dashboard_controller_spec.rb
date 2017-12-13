9911f6f912ce513c1622d16da7cee1e68564ab89
AmirolAhmad <amirol@lizard-apps.com>
2017-12-13 16:40:49 +0800

    make rspec passed

--- spec/controllers/dashboard/dashboard_controller_spec.rb
+++ spec/controllers/dashboard/dashboard_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::DashboardController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:valid_session) { sign_in(user) }
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    describe "when user is logged in" do
+      before { valid_session }
+
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "when user is not logged in" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:redirect)
+      end
+      it " will redirect to login url" do
+        get :index
+        expect(response).to redirect_to new_user_session_url
+      end
     end
   end
-
 end
