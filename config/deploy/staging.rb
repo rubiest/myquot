0f9f0830cd01f0951bf1180b569623957a864567
AmirolAhmad <amirol@lizard-apps.com>
2017-12-28 19:52:46 +0800

    change to ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,4 @@ set :rails_env, :staging
 set :deploy_to, '/home/deploy/myquot_staging'
 set :release_note_url, 'http://staging.myquot.com/release_note.txt'
 
-server 'myquot.com', user: 'deploy', roles: %w{web app db job}
+server '52.221.37.206', user: 'deploy', roles: %w{web app db job}
