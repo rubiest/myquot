8780dc547b785862dfcece66391342c2c0dd5419
AmirolAhmad <amirol@lizard-apps.com>
2017-12-28 19:47:05 +0800

    change name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,4 @@ set :rails_env, :staging
 set :deploy_to, '/home/deploy/myquot_staging'
 set :release_note_url, 'http://staging.myquot.com/release_note.txt'
 
-server 'staging.myquot.com', user: 'deploy', roles: %w{web app db job}
+server 'myquot.com', user: 'deploy', roles: %w{web app db job}
